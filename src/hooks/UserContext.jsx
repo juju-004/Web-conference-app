5e3e7f550751fec421cb699f641bc3933d4918aa
David <udohdavid004@gmail.com>
2024-08-12 19:30:04 -0700

    fix

--- src/hooks/UserContext.jsx
+++ src/hooks/UserContext.jsx
@@ -3,10 +3,10 @@
 import React, { useState } from "react";
 import { createContext } from "react";
 
-const Context = createContext();
+const Context = createContext(null);
 
 export default function UserContext({ children }) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
 
   return (
     <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
